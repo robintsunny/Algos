03d158d8edca0f8ce866b76c7f166ad49c79e0ed
robin <robintsunny@gmail.com>
2019-04-08 11:22:34 -0700

    palindromes

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,14 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((el,i) => {
+        return el === str[str.length-i-1]
+    });
+}
+
+// function palindrome(str) {
+//     return str === str.split('').reverse().join('');
+// }
 
 module.exports = palindrome;
