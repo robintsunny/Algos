6f0a4f2caa31f82e20c4a4fabb5c8329dbbd6cf4
robin <robintsunny@gmail.com>
2019-04-09 12:35:08 -0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {};
+
+    str.split('').forEach(i => {
+        obj[i] = obj[i]+1 || 1;
+    })
+
+    const keys = Object.keys(obj)
+    const sortable = [];
+
+    return keys.sort((a,b) => {obj[b]-obj[a]})[0];
+}
+
+
+maxChar('abcdefghijklmnaaaaa')
 
 module.exports = maxChar;
