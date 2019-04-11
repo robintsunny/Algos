981e07935c61fc1a43738fdeb603a01719a37353
robin <robintsunny@gmail.com>
2019-04-11 11:25:21 -0700

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,30 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    // returns array if matches exists or null if no matches were made
+    // g option captures all instances, not just the first
+    // i is for case insensitive
+
+    return matches ? matches.length : 0
+}
+// function vowels(str) {
+//     let res = 0;
+
+//     str.toLowerCase().split('').forEach(letter => {
+//         if (
+//             letter === 'a' ||
+//             letter === 'e' ||
+//             letter === 'i' ||
+//             letter === 'o' ||
+//             letter === 'u'
+//         ) {
+//             res++
+//         }
+//     })
+
+//     return res
+// }
 
 module.exports = vowels;
