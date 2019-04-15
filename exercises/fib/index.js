e514cf4387eaf3c0435f39d355fc24a511f6b332
robin <robintsunny@gmail.com>
2019-04-15 11:29:29 -0700

    fibonacci: iterative, recersive, and memoized

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,43 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args]
+        } 
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
+
+
+function slowFib(n) {
+    if (n === 0) {
+        return 0
+    } else if (n === 1) {
+        return 1
+    }
+
+    return fib(n-2) + fib(n-1) //slowfib has to call the memoized version
+}
+    
+const fib = memoize(slowFib)
+
+// function fib(n) {
+//     const res = [0,1]
+
+//     while (res.length <= n) {
+//         res.push(res[res.length-1] + res[res.length-2])
+//     }
+
+//     return res[n]
+// }
 
 module.exports = fib;
