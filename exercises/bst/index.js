66e4e00a0793b8be34a036c8396e568e03693753
robin <robintsunny@gmail.com>
2019-04-24 18:09:08 -0700

    binary search tree recursive

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -20,39 +20,63 @@ class Node {
     }
 
     insert(data) {
-        let node = this;
-        let newNode = new Node();
-        newNode.data = data;
+        // let node = this;
+        // let newNode = new Node();
+        // newNode.data = data;
 
-        while (node) {
-            if (data < node.data) {
-                if (node.left) {
-                    node = node.left
-                    continue
-                } else {
-                    node.left = newNode
-                    break
-                }
-            } else {
-                if (node.right) {
-                    node = node.right
-                    continue
-                } else {
-                    node.right = newNode
-                    break
-                }
-            }
+        // while (node) {
+        //     if (data < node.data) {
+        //         if (node.left) {
+        //             node = node.left
+        //             continue
+        //         } else {
+        //             node.left = newNode
+        //             break
+        //         }
+        //     } else {
+        //         if (node.right) {
+        //             node = node.right
+        //             continue
+        //         } else {
+        //             node.right = newNode
+        //             break
+        //         }
+        //     }
+        // }
+
+        if (data < this.data && this.left) {
+            this.left.insert(data)
+        } else if (data < this.data) {
+            this.left = new Node(data)
+        } else if (data > this.data && this.right) {
+            this.right.insert(data)
+        } else if (data > this.data) {
+            this.right = new Node(data)
         }
-
     }
 
     contains(data) {
-        let node = this;
+        // let node = this;
 
-        while (node) {
-            if (node.data === data) {return node}
-            if (data < node.data) {node = node.left}
-            if (data > node.data) {node = node.right}
+        // while (node) {
+        //     if (node.data === data) {return node}
+        //     if (data < node.data) {node = node.left}
+        //     if (data > node.data) {node = node.right}
+        // }
+
+        // return null
+
+
+        if (this.data === data) {
+            return this
+        }
+
+        if (data < this.data && this.left) {
+            return this.left.contains(data)
+        } 
+
+        if (data > this.data && this.right) {
+            return this.right.contains(data)
         }
 
         return null
