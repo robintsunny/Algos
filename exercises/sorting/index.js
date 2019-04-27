e52f87e4189c3b85f20313d392c8ce7032cd0ef6
robin <robintsunny@gmail.com>
2019-04-26 18:13:53 -0700

    selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -18,7 +18,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+    let indexOfMin;
+    
+    for (let i = 0; i < arr.length; i++) {
+        indexOfMin = i
+        for (let j = i+1; j<arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j
+            }
+        }
 
+        if (indexOfMin !== i) {
+            [arr[i], arr[indexOfMin]] = [arr[indexOfMin], arr[i]]
+        }
+    }
+
+    return arr
 }
 
 function mergeSort(arr) {
