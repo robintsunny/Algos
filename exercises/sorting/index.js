8af319ff33ccfeafaff67aa1f05d572fd10732d2
robin <robintsunny@gmail.com>
2019-04-26 18:25:53 -0700

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -37,10 +37,30 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr
+    }
+
+    const mid = Math.floor(arr.length/2);
+    const left = arr.slice(0,mid)
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right))
+
 
 }
 
 function merge(left, right) {
+    const res = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            res.push(left.shift())
+        } else if (left[0] > right[0]) {
+            res.push(right.shift())
+        }
+    }
+
+    return [...res, ...left, ...right]
 
 }
 
